f502a5bb844485607ad96b328fdefc744e400dba
Sargis Mkhitaryan <sargismk@icloud.com>
2018-09-23 23:46:30 +0400

    Add event main images.

--- Example/AnimateTableView/ViewControllers/EventsViewController.swift
+++ Example/AnimateTableView/ViewControllers/EventsViewController.swift
@@ -34,11 +34,11 @@ class EventsViewController: UIViewController {
     // MARK: - Private Methods
     
     private func setupEvents() {
-        let event1 = Event(title: "Theater", date: Date(), address: "Yerevan", thumbnailImage: ".png", mainImage: ".png")
-        let event2 = Event(title: "Open Handed Revolution", date: Date(), address: "Yerevan", thumbnailImage: ".png", mainImage: ".png")
-        let event3 = Event(title: "Cake Party", date: Date(), address: "Dubai", thumbnailImage: ".png", mainImage: ".png")
-        let event4 = Event(title: "Celebration Concert", date: Date(), address: "Loc Angeles", thumbnailImage: ".png", mainImage: ".png")
-        let event5 = Event(title: "Band Bright", date: Date(), address: "London", thumbnailImage: ".png", mainImage: ".png")
+        let event1 = Event(title: "Theater", date: Date(), address: "Yerevan", thumbnailImage: ".png", mainImage: "audience-crowd-event.png")
+        let event2 = Event(title: "Open Handed Revolution", date: Date(), address: "Yerevan", thumbnailImage: ".png", mainImage: "audience-blur-bokeh.png")
+        let event3 = Event(title: "Cake Party", date: Date(), address: "Dubai", thumbnailImage: ".png", mainImage: "architecture-auditorium-blue.png")
+        let event4 = Event(title: "Celebration Concert", date: Date(), address: "Loc Angeles", thumbnailImage: ".png", mainImage: "audience-celebration-concert.png")
+        let event5 = Event(title: "Band Bright", date: Date(), address: "London", thumbnailImage: ".png", mainImage: "audience-band-bright.png")
         let events = [event1, event2, event3, event4, event5]
         eventsViewModels = events.map { EventViewModel(event: $0) }
         tableView.animate()
