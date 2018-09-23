4d6aced316081933394ac15d6630bc946b2ad939
Sargis Mkhitaryan <sargismk@icloud.com>
2018-09-24 00:04:00 +0400

    Add events thumbnail images.

--- Example/AnimateTableView/ViewControllers/EventsViewController.swift
+++ Example/AnimateTableView/ViewControllers/EventsViewController.swift
@@ -25,7 +25,6 @@ class EventsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let cell =
         tableView.register(UINib(nibName: "EventTableViewCell", bundle: nil), forCellReuseIdentifier: EventsViewController.eventCellId)
         
         setupEvents()
@@ -34,11 +33,31 @@ class EventsViewController: UIViewController {
     // MARK: - Private Methods
     
     private func setupEvents() {
-        let event1 = Event(title: "Theater", date: Date(), address: "Yerevan", thumbnailImage: ".png", mainImage: "audience-crowd-event.png")
-        let event2 = Event(title: "Open Handed Revolution", date: Date(), address: "Yerevan", thumbnailImage: ".png", mainImage: "audience-blur-bokeh.png")
-        let event3 = Event(title: "Cake Party", date: Date(), address: "Dubai", thumbnailImage: ".png", mainImage: "architecture-auditorium-blue.png")
-        let event4 = Event(title: "Celebration Concert", date: Date(), address: "Loc Angeles", thumbnailImage: ".png", mainImage: "audience-celebration-concert.png")
-        let event5 = Event(title: "Band Bright", date: Date(), address: "London", thumbnailImage: ".png", mainImage: "audience-band-bright.png")
+        let event1 = Event(title: "Theater",
+                           date: Date(),
+                           address: "Yerevan",
+                           thumbnailImage: "audience-crowd-event-thumbnail.png",
+                           mainImage: "audience-crowd-event.png")
+        let event2 = Event(title: "Open Handed Revolution",
+                           date: Date(),
+                           address: "Yerevan",
+                           thumbnailImage: "audience-blur-bokeh-thumbnail.png",
+                           mainImage: "audience-blur-bokeh.png")
+        let event3 = Event(title: "Cake Party",
+                           date: Date(),
+                           address: "Dubai",
+                           thumbnailImage: "architecture-auditorium-blue-thumbnail.png",
+                           mainImage: "architecture-auditorium-blue.png")
+        let event4 = Event(title: "Celebration Concert",
+                           date: Date(),
+                           address: "Loc Angeles",
+                           thumbnailImage: "audience-celebration-concert-thumbnail.png",
+                           mainImage: "audience-celebration-concert.png")
+        let event5 = Event(title: "Band Bright",
+                           date: Date(),
+                           address: "London",
+                           thumbnailImage: "audience-band-bright-thumbnail.png",
+                           mainImage: "audience-band-bright.png")
         let events = [event1, event2, event3, event4, event5]
         eventsViewModels = events.map { EventViewModel(event: $0) }
         tableView.animate()
