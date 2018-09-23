da97ea6fe5f41801823cc9f5645f90b5a4b06f8d
Sargis Mkhitaryan <sargismk@icloud.com>
2018-09-23 23:28:57 +0400

    Add details label information on event cell

---
+++ Example/AnimateTableView/Views/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  AnimateTableView
+//
+//  Created by Sargis on 9/23/18.
+//  Copyright © 2018 Sargis. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
